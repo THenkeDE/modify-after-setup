7a7df0fd1fa4cc066fac8c58c1379578863dafa7
THenke <smartgit@thenke.de>
2021-04-13 15:12:12 +0200

    + basic config gui

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -5,14 +5,54 @@ class IndexController extends pm_Controller_Action
 
 	private $Meta = null;
 
-	public function indexAction()
+	public function init()
 	{
+		parent::init();
 		$this->Meta = $this->loadMetaXml();
 		$this->view->pageTitle = $this->lmsg('pagetitle',[
 			'extension' => $this->Meta->name,
 			'version' => '<small>(v' . $this->Meta->version.'-'.$this->Meta->release.'</small>)',
 			'icon' => pm_Context::getBaseUrl() . 'images/new-php-logo.svg' 
 		]);
+	}
+
+	public function indexAction()
+	{
+		$this->_forward('config');
+	}
+
+	public function configAction()
+	{
+
+		$form = new pm_Form_Simple();
+		$form->addElement('text', 'basefoldername', [
+			'label'		=> $this->lmsg('form_basefoldername_label'),
+			'placeholder'	=> $this->lmsg('form_basefoldername_placeholder'),
+			'escape'	=> false,
+			'value'		=> pm_Settings::get('basefoldername'),
+		]);
+		$form->addElement('select', 'seperatefolder', [
+			'label'		=> $this->lmsg('form_seperatefolder_label'),
+			'multiOptions'	=> ['1' => 'yes', '0' => 'no'],
+			'value'		=> pm_Settings::get('seperatefolder'),
+			'required'	=> true,
+		]);
+
+		$form->addControlButtons([
+			'sendTitle'	=> $this->lmsg('form_save_config'),
+			'cancelHidden'	=> true,
+			'hideLegend'	=> true,
+			'withSeparator'	=> false
+		]);
+
+		if ($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getPost())) {
+			pm_Settings::set('basefoldername', $form->getValue('basefoldername'));
+			pm_Settings::set('seperatefolder', $form->getValue('seperatefolder') );
+			$this->_status->addMessage('info', $this->lmsg('form_save_success'));
+			$this->_helper->json(['redirect' => pm_Context::getBaseUrl()]);
+		}
+
+		$this->view->form = $form;
 	}
 
 	public function loadMetaXml()
--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -3,6 +3,11 @@
 	$messages = [
 		'pagetitle' => '<img src="%%icon%%" style="width:88px;height:88px;vertical-align: middle;" /> Sessions %%version%%',
 		'navigation_admin_title' => 'PHP-Sessions',
-		'navigation_admin_description' => 'Configure PHP Session Savepath'
+		'navigation_admin_description' => 'Configure PHP Session Savepath',
+		'form_save_config' => 'Save config',
+		'form_save_success' => 'Configuration updated successfully',
+		'form_basefoldername_label' => 'Sessionfolder',
+		'form_basefoldername_placeholder' => '',
+		'form_seperatefolder_label' => 'Use seperate folder',
 	];
 
