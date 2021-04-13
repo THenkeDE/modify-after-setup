4d6c5109e3bba737394d99e163ed0734ca6a9087
THenke <smartgit@thenke.de>
2021-04-13 15:19:56 +0200

    + german locale

---
+++ plib/resources/locales/de-DE.php
@@ -0,0 +1,13 @@
+<?php
+
+	$messages = [
+		'pagetitle' => '<img src="%%icon%%" style="width:88px;height:88px;vertical-align: middle;" /> Sessions %%version%%',
+		'navigation_admin_title' => 'PHP-Sessions',
+		'navigation_admin_description' => 'PHP Sitzungsdateien (Sessions) Speicherort',
+		'form_save_config' => 'Konfiguration speichern',
+		'form_save_success' => 'Konfiguration wurde erfolgreich gespeichert',
+		'form_basefoldername_label' => 'Ordner für Sitzungsdateien',
+		'form_basefoldername_placeholder' => '',
+		'form_seperatefolder_label' => 'Ordner pro Domain',
+	];
+
